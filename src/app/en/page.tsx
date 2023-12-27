9ac3ded377d4423aa6ab27a8e80c5fbd4d5740d5
OtabekVaxobov <elot2017@mail.ru>
2023-12-27 14:37:02 +0500

    fix: fix

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -11,60 +11,53 @@ import bg from "../../../public/page-bg.jpg";
 import Image from "next/image";
 
 export default function EnPage() {
-    return (
-        <>
+  return (
+    <>
+      <Header2 />
+      <Image
+        className="hidden md:flex"
+        width={0}
+        height={0}
+        src={bg}
+        alt="background_picture"
+      />
+      <div className="flex flex-col w-full bg-gradient-to-r from-red-400  to-blue-600 p-4 py-20 sm:px-20 sm:flex-row justify-between items-center">
+        <p className=" text-white">
+          <span className="text-4xl font-medium">Want to contact us?</span>
+          <br />
+          <span className="text-lg">Contact now!</span>
+        </p>
+        <div className="font-medium  pt-4  dark:text-white">
+          <div className=" text-white grid gap-4">
+            <h4 className="font-medium leading-none">Fax:</h4>
+            <p className="text-white text-sm text-muted-foreground">
+              67-377-21-75
+            </p>
+          </div>
+          <div className="text-white grid gap-4">
+            <h4 className="font-medium  pt-4 leading-none"> Reception </h4>
+            <p className=" text-white text-sm text-muted-foreground">
+              67-377-22-41
+            </p>
+          </div>
+        </div>
+      </div>
 
-            <Header2 lang='ru' />
-            <Image
-                className="hidden md:flex"
-                width={0}
-                height={0}
-                src={bg}
-                alt="background_picture" />
-            <div className="flex flex-col w-full bg-gradient-to-r from-red-400  to-blue-600 p-4 py-20 sm:px-20 sm:flex-row justify-between items-center">
-                <p className=" text-white">
-                    <span className="text-4xl font-medium">Want to contact us?</span>
-                    <br />
-                    <span className="text-lg">Contact now!</span>
-                </p>
-                <div className="font-medium  pt-4  dark:text-white">
+      <main className="flex min-h-screen max-w-auto flex-col items-center justify-between sm:p-24">
+        <Carusel />
+        <About />
+        <Gallery />
+        {/* <Gallery2 /> */}
+        <Suspense>
+          <h2 className="text-3xl py-8">News</h2>
+          <GetNews />
+        </Suspense>
 
+        <Questions />
 
+        <Gallery3 />
 
-                    <div className=" text-white grid gap-4">
-                        <h4 className="font-medium leading-none">Fax:</h4>
-                        <p className="text-white text-sm text-muted-foreground">
-                            67-377-21-75
-                        </p>
-                    </div>
-                    <div className="text-white grid gap-4">
-                        <h4 className="font-medium  pt-4 leading-none"> Reception </h4>
-                        <p className=" text-white text-sm text-muted-foreground">
-                            67-377-22-41
-                        </p>
-                    </div>
-
-                </div>
-            </div>
-
-
-            <main className="flex min-h-screen max-w-auto flex-col items-center justify-between sm:p-24">
-                <Carusel />
-                <About />
-                <Gallery />
-                {/* <Gallery2 /> */}
-                <Suspense>
-                    <h2 className="text-3xl py-8">News</h2>
-                    <GetNews />
-                </Suspense>
-
-                <Questions />
-
-
-                <Gallery3 />
-
-
-                {/* <h2 className=" pt-8 text-3xl">Meyoriy hujjatlar</h2>
+        {/* <h2 className=" pt-8 text-3xl">Meyoriy hujjatlar</h2>
         <section id="hujjatlar" className="flex flex-row justify-center gap-4 py-8">
           <Link target="_blank" rel="noopener noreferrer" download href='https://drive.google.com/file/d/1MOrcmzP1SAvMn2UugMoCwEQ9ri5ePABn/view?usp=sharing' title="Tashkilot Protokollari">
             <Image src={Protokol} alt={"Protokol"} width={100} height={100} />
@@ -83,8 +76,8 @@ export default function EnPage() {
             <Image src={Klinik} alt={"Klinik"} width={120} height={120} />
           </Link>
         </section> */}
-            </main>
-            <Footer2 />
-        </>
-    )
+      </main>
+      <Footer2 />
+    </>
+  );
 }
